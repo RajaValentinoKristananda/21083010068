4d68ea1e1fc70d9c7d1ea854ef6fae7b1def539d
RajaValentinoKristananda <112999305+RajaValentinoKristananda@users.noreply.github.com>
2022-11-02 16:09:29 +0700

    Add files via upload

---
+++ Tugas_7.sh
@@ -0,0 +1,19 @@
+panjang() {
+   echo "Masukkan Panjang :"
+   read panjang
+}
+
+lebar() {
+   echo "Masukkan Lebar :"
+   read lebar
+}
+
+luas() {
+   echo "Program Menghitung Luas Bidang Persegi"
+   panjang
+   lebar
+   let luas=$panjang*$lebar
+   echo "Luas Persegi : $luas"
+}
+
+luas
